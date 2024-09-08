183d10969e97e8fea102effe1d9d9fe92de9d1da
git <yokofujishiro.0418@gmail.com>
2024-09-08 15:13:37 +0900

    [fix]try endpoint

--- apps/minimalapp/app.py
+++ apps/minimalapp/app.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return "Hello, Flaskbook!"
+@app.route("/hello/<int:name>",methods=["GET"],endpoint="hello-endpoint")
+def hello(name):
+    return f"Hello, {name}!"
+
+@app.route("/name/<name>")
+def show_name(name):
+    return render_template("index.html", name=name)
